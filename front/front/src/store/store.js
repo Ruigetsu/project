3df0bbdd3006e01c2d2ea2426ef378add4cd8a1d
Lisa <e.gryumova@yandex.ru>
2024-03-16 13:18:24 +0300

    add token add, token reduser

--- front/front/src/store/store.js
+++ front/front/src/store/store.js
@@ -2,9 +2,11 @@ import { configureStore } from '@reduxjs/toolkit'
 import { combineReducers } from 'redux';
 
 import walletReducer from './walletReducer.js';
+import tokenReducer from './TokenReducer.js';
 
 const rootReducer = combineReducers({
-    wallet: walletReducer
+    wallet: walletReducer,
+    token: tokenReducer
 });
 
 export const store = configureStore({
