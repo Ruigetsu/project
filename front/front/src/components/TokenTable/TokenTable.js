b377d33cf727ed3e1a9678142cea6552311aba68
Anton <dynovskiyanton@gmail.com>
2024-03-17 20:39:37 +0300

    .

--- front/front/src/components/TokenTable/TokenTable.js
+++ front/front/src/components/TokenTable/TokenTable.js
@@ -3,26 +3,14 @@ import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the 
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
 import { useSelector } from 'react-redux';
 import "./TokenTable.css";
-<<<<<<< HEAD
 import { useEffect, useState} from "react";
-=======
->>>>>>> 4ff35137447bb2887e8f51207021bf4d3d56d921
 import { BalanceFormatter, PriceFormatter, ValueRenderer } from './value_renderer';
 import LastUpdate from './menu_item';
 
 function TokenTable () {
-<<<<<<< HEAD
     const balance = useSelector((state) => state.token);
-    const [rowData, setRowData] = useState(balance);
-    
-    useEffect(() => {
-        setRowData(balance);
-    }, [balance]);
-    
-=======
-    const tokens = useSelector(state => state.token);
 
->>>>>>> 4ff35137447bb2887e8f51207021bf4d3d56d921
+    
     const columnDefs = [
         { field: 'wallet_name', headerName: 'wallet', cellClass: ['celClass', 'celClassLeft']},
         { field: 'asset', headerName: 'asset', cellClass: ['celClass', 'celClassLeft'] },
@@ -50,11 +38,7 @@ function TokenTable () {
          style={{ height: 500 }} // the grid will fill the size of the parent container
         >
           <AgGridReact
-<<<<<<< HEAD
               // rowData={balance}
-=======
-              rowData={tokens}
->>>>>>> 4ff35137447bb2887e8f51207021bf4d3d56d921
               columnDefs={columnDefs}
           />
         </div>
--- front/front/src/index.js
+++ front/front/src/index.js
@@ -1,33 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 
-=======
-import WalletList from './components/WalletList/WalletList';
->>>>>>> 4ff35137447bb2887e8f51207021bf4d3d56d921
 
 import {store} from './store/store.js';
 import { Provider } from 'react-redux';
 import "./index.css";
-<<<<<<< HEAD
 
 import App from './App/App.js'
-=======
-import AddWallet from './components/AddWallet/AddWallet.js';
-import AddToken from './components/AddToken/AddToken.js';
-import TokenTable from './components/TokenTable/TokenTable.js';
->>>>>>> 4ff35137447bb2887e8f51207021bf4d3d56d921
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-<<<<<<< HEAD
       <App/>
-=======
-      <WalletList/>
-      <AddWallet/>
-      <AddToken/>
-      <TokenTable/>
->>>>>>> 4ff35137447bb2887e8f51207021bf4d3d56d921
   </Provider>
 );
