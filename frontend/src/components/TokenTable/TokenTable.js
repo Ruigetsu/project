7816369d7b3a7a90211ec50761ca3c4f81d7e0d8
Anton <dynovskiyanton@gmail.com>
2024-03-28 20:37:39 +0300

    traefik and docker-compose

--- frontend/src/components/TokenTable/TokenTable.js
+++ frontend/src/components/TokenTable/TokenTable.js
@@ -2,6 +2,7 @@ import { AgGridReact } from 'ag-grid-react'; // AG Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the grid
 import { useSelector } from 'react-redux';
+import { useMemo } from 'react';
 import "./TokenTable.css";
 import { BalanceFormatter, PriceFormatter, ValueRenderer } from './value_renderer';
 import LastUpdate from './menu_item';
@@ -30,7 +31,19 @@ function TokenTable () {
             cellRenderer: LastUpdate
         }
     ];
-
+    const defaultColDef = useMemo(() => {
+      return {
+        editable: true,
+        enableRowGroup: true,
+        enablePivot: true,
+        enableValue: true,
+        sortable: true,
+        resizable: true,
+        filter: true,
+        flex: 1,
+        minWidth: 40,
+      };
+  }, []);
     return (
         <div
          className="ag-theme-quartz" // applying the grid theme
@@ -39,6 +52,7 @@ function TokenTable () {
           <AgGridReact
             rowData={balance}
             columnDefs={columnDefs}
+            defaultColDef={defaultColDef}
           />
         </div>
       )
